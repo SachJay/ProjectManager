324b5721593519fdb63cf8f7b4ed2e6471609241
SachJay <jayaweerasachin@gmail.com>
2019-12-22 13:56:03 -0500

    interface will display tasks based on database

--- src/App.js
+++ src/App.js
@@ -6,6 +6,26 @@ var Tasks = ({task="Undefined"}) => (
   <h1>{task}</h1>
 );
 
+var SprintColumn = ({title="To Do", tasks=[]}) => (
+  <div className="item col-md-3">
+    <div className="col-md-12 bg-danger pb-1 rounded-lg">
+      <h2>{title}</h2>
+      {
+        tasks.map((task) =>
+          <SprintRow title={task.title}/>
+        )
+      }
+    </div>
+  </div>
+);
+
+var SprintRow = ({title="Interface", desc="Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude."}) => (
+  <div className="col-12 bg-warning mb-4 rounded-lg">
+    <h4>{title}</h4>
+     {desc}
+  </div>
+);
+
 class App extends Component {
   constructor(){
     super();
@@ -18,16 +38,19 @@ class App extends Component {
       open: false,
       isUpdated: true,
       isFetching: false,
-      data: [{"task": "Interface"},{"task": "Design"},{"task": "Backend"}]
+      toDoTasks: [{"title": "Backend"},{"title": "Database"}],
+      doingTasks: [{"title": "Interface"}],
+      reviewTasks: [{"title": "Design"}],
+      doneTasks: []
     };
   }
 
   renderTasks = () => {
     return (
       <ul>
-        {this.state.data.map((task, id) =>
+        {this.state.toDoTasks.map((task, id) =>
           <li key={id}>
-            <Tasks task={task.task}/>
+            <Tasks task={task.title}/>
           </li>
         )}
       </ul>
@@ -74,60 +97,20 @@ class App extends Component {
     
 
     return (
-     
       <div className="container my-5">
-          <div className="row">
-
-            <div className="item col-md-3">
-              <div className="col-md-12 bg-danger pb-1 rounded-lg">
-                <h2>To do</h2>
-                <div className="col-12 bg-warning mb-4 rounded-lg">
-                  <h4>Interface</h4>
-                  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas 
-                </div>
-               
-                <div className="col-12 bg-warning mb-4 rounded-lg">
-                 <h4>Interface</h4>
-                  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas 
-                </div>
-
-                <div className="col-12 bg-warning mb-4 rounded-lg">
-                  <h4>Interface</h4>
-                  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas 
-                </div>
-              </div>
-            </div>
-
-            <div className="col-md-3">
-              <div className="col-md-12 bg-danger pb-1">
-                <h2>Doing</h2>
-                EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas
-              </div>
-            </div>
-
-            <div className="col-md-3">
-              <div className="col-md-12 bg-danger pb-1">
-                <h2>Review</h2>
-                EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas
-              </div>
-            </div>
-
-            <div className="col-md-3">
-              <div className="col-md-12 bg-danger pb-1">
-                <h2>Done</h2>
-                EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas  EDfs ada gead stgaes sadas dadsad sdasdsa dsad asdasda dasda sdas dasda sdas
-              </div>
-            </div>
-
-          </div>
-          
-        
         <h2>This Bug Tracker is {this.state.open ? 'Online' : 'Offline'}</h2>
 
-        <h3>Remaining Tasks</h3>
+        <div className="row">
+          <SprintColumn title="To Do" tasks={this.state.toDoTasks}></SprintColumn>
+          <SprintColumn title="Doing" tasks={this.state.doingTasks}></SprintColumn>
+          <SprintColumn title="Review" tasks={this.state.reviewTasks}></SprintColumn>
+          <SprintColumn title="Done" tasks={this.state.doneTasks}></SprintColumn>
+        </div>
+          
         <input ref={this.textInput}></input>
         <br></br>
         <button onClick={this.onButtonClick}>Submit</button>
+
         {this.state.isUpdated ? this.getData() : this.addTask()}
       </div>
     )
