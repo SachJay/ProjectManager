5a252d7aea80d6f589ad74e09b0cc1821814f13a
SachJay <jayaweerasachin@gmail.com>
2019-12-24 14:33:05 -0500

    added 'add task button' and created template to add tasks

--- src/App.js
+++ src/App.js
@@ -9,20 +9,42 @@ var Tasks = ({task="Undefined"}) => (
 var SprintColumn = ({title="To Do", tasks=[]}) => (
   <div className="item col-md-3">
     <div className="col-md-12 bg-danger pb-1 rounded-lg">
-      <h2>{title}</h2>
+      <h1 className="text-center pb-4">{title}</h1>
       {
         tasks.map((task) =>
           <SprintRow title={task.title}/>
         )
       }
+      <AddPanel></AddPanel>
+      <AddButton></AddButton>
     </div>
   </div>
 );
 
 var SprintRow = ({title="Interface", desc="Unwilling sportsmen he in questions september therefore described so. Attacks may set few believe moments was. Reasonably how possession shy way introduced age inquietude."}) => (
-  <div className="col-12 bg-warning mb-4 rounded-lg">
-    <h4>{title}</h4>
+  <div className="col-12 bg-warning mb-4 rounded-lg pb-3 pt-2">
+    <h4 className="text-center">{title}</h4>
      {desc}
+  </div>
+);
+
+var AddButton = () => (
+  <button className="col-md-12 bg-warning pb-1 rounded-lg border-0 text-white mb-4">Add New Task</button>
+);
+
+var AddPanel = () => (
+  <div className=" bg-warning rounded-lg mb-4 py-3 ">
+    <div className=" col-12 input-group-sm">
+      <textarea rows="1" className="mb-3 form-control" placeholder="Title" type="text"></textarea>
+    
+      <textarea rows="3" className="mb-3 form-control" placeholder="Description" type="text"></textarea>
+    </div>
+
+    <div className="col-12">
+      <button className="col-12 bg-info pb-1 rounded-lg border-0 text-white mb-2">Add Task</button>
+    </div>
+    
+
   </div>
 );
 
@@ -32,8 +54,9 @@ class App extends Component {
 
     var nextId = 0;
 
-    this.textInput = React.createRef();
-    
+    this.textInputTitle = React.createRef();
+    this.textInputInfo = React.createRef();
+
     this.state = {
       open: false,
       isUpdated: true,
@@ -62,9 +85,7 @@ class App extends Component {
     // open the request with the verb and the url
     xhr.open('POST', 'http://localhost:3000/data', true);
     xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
-    xhr.send(JSON.stringify({"task": this.textInput.current.value}));
-    
-    console.log(JSON.stringify({"task": this.textInput.current.value}));
+    xhr.send(JSON.stringify({"title": this.textInputTitle.current.value, desc: this.textInputInfo.current.value}));
   }
 
   onButtonClick = () => {
@@ -107,7 +128,9 @@ class App extends Component {
           <SprintColumn title="Done" tasks={this.state.doneTasks}></SprintColumn>
         </div>
           
-        <input ref={this.textInput}></input>
+        <input ref={this.textInputTitle}></input>
+        <br></br>
+        <input ref={this.textInputInfo}></input>
         <br></br>
         <button onClick={this.onButtonClick}>Submit</button>
 
